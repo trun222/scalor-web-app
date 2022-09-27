94985fa6c61cd28b09bdbb231d2d22b71ade760f
trun222 <thomasunderwoodii@gmail.com>
2022-09-27 09:47:05 -0400

    Improved landing background image

--- src/components/LandingSection.tsx
+++ src/components/LandingSection.tsx
@@ -18,9 +18,6 @@ const LandingSection = () => {
         <VStack align="left" justify="center" h="80%" spacing={4} padding={6} bg='rgba(0,0,0,.6)' borderRadius="md">
           <Text fontSize="5xl" fontWeight="bold" color="white">The Image API that makes app development fun</Text>
           <Text fontSize="2xl" fontWeight="semibold" color="white">Focus on your code, not resizing images.</Text>
-
-
-
           <Text fontSize="2xl" color="white">Sign Up for the <b>Beta</b>!</Text>
 
           <HStack spacing={4} mt={6}>
