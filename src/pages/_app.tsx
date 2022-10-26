e9f14767f0fc6fba1da981111139a933a71d66ce
trun222 <thomasunderwoodii@gmail.com>
2022-10-26 09:19:07 -0400

    Added Hotjar

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,18 @@
 import '../styles/globals.css'
 import { ChakraProvider } from '@chakra-ui/react'
 import { SessionProvider } from 'next-auth/react';
-import React from "react";
+import React, { useEffect } from "react";
+import Hotjar from '@hotjar/browser';
 
 function MyApp({ Component, session, pageProps }: any) {
+  const siteID: any = process?.env?.NEXT_PUBLIC_HOTJAR_SITE_ID!;
+
+  useEffect(() => {
+    Hotjar.init(siteID, 6, {
+      debug: true
+    });
+  });
+
   return (
     <SessionProvider session={session}>
       <ChakraProvider>
