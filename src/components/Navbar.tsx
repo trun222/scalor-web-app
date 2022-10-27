20274cf234b9f4f5de9c049e166d9384f556b2f3
trun222 <thomasunderwoodii@gmail.com>
2022-10-26 22:41:41 -0400

    Made Navbar responsive

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,13 +1,17 @@
 import React from 'react';
-import { Text, Button, HStack, Box } from '@chakra-ui/react'
+import {
+  Text, Button, HStack, Box, Menu, MenuItem, MenuList, MenuButton, IconButton, Flex
+} from '@chakra-ui/react'
 import NextLink from 'next/link';
 import Image from 'next/image';
 import { useSession, signIn, signOut } from 'next-auth/react';
+import { MdMenu, MdMenuBook, MdCode, MdHelpCenter, MdLogout, MdLogin } from "react-icons/md";
 
 export default function Navbar() {
   const { data: session } = useSession();
 
-  const UserSession = () => {
+
+  const DesktopUserSession = () => {
     if (session) {
       return (
         <HStack>
@@ -33,57 +37,170 @@ export default function Navbar() {
     )
   }
 
-  return (
-    <HStack alignItems="center" justify="space-between" h="100%">
-      <HStack spacing={4}>
-        <NextLink href='/' passHref>
-          <Text
-            fontSize="4xl"
-            letterSpacing={10}
-            _hover={{
-              color: 'gray.600'
-            }}
-            cursor="pointer"
-          >
-            Scalor
-          </Text>
-        </NextLink>
-        <NextLink href='/' passHref>
-          <Text
-            fontSize="4xl"
-            color='red.400'
-            fontWeight="bold"
-            cursor="pointer"
-          >
-            (BETA)
-          </Text>
-        </NextLink>
+  const MobileNav = () => {
+    return (
+      <HStack alignItems="center" justify="space-between" h="100%">
+        <Flex>
+          <NextLink href='/' passHref>
+            <Text
+              fontSize="4xl"
+              letterSpacing={10}
+              _hover={{
+                color: 'gray.600'
+              }}
+              cursor="pointer"
+            >
+              Scalor
+            </Text>
+          </NextLink>
+          <NextLink href='/' passHref>
+            <Text
+              fontSize="4xl"
+              color='red.400'
+              fontWeight="bold"
+              cursor="pointer"
+            >
+              (BETA)
+            </Text>
+          </NextLink>
+        </Flex>
 
-        <NextLink href='/docs/upload' passHref>
-          <Button bg="none">
-            Documentation
-          </Button>
-        </NextLink>
-        <NextLink href='/demo/resize' passHref>
-          <Button bg="none">
-            Demo
-          </Button>
-        </NextLink>
-        {/* <NextLink href='/pricing' passHref>
+        <Menu>
+          <MenuButton
+            as={IconButton}
+            aria-label='Options'
+            icon={<MdMenu />}
+            variant='outline'
+          />
+          <MenuList p={0}>
+            <MenuItem icon={<MdMenuBook fontSize="26px" />}>
+              <NextLink href='/docs/upload' passHref>
+                Documentation
+              </NextLink>
+            </MenuItem>
+            <MenuItem icon={<MdCode fontSize="26px" />}>
+              <NextLink href='/demo/resize' passHref>
+                Demo
+              </NextLink>
+            </MenuItem>
+            <MenuItem icon={<MdHelpCenter fontSize="26px" />}>
+              <NextLink href='mailto:scalor@protonmail.com'>
+                Support
+              </NextLink>
+            </MenuItem>
+
+
+            {session &&
+              <MenuItem>
+                <NextLink href='/dashboard' passHref>
+                  <HStack align="center" justify="start" spacing={4} h="100%" w="100%">
+                    <Box h="26px" w="26px">
+                      <Image
+                        src={session?.user?.image!}
+                        alt="Profile"
+                        height="26px"
+                        width="26px"
+                      />
+                    </Box>
+                    <Text>Dashboard</Text>
+                  </HStack>
+                </NextLink>
+              </MenuItem>
+            }
+
+            {session &&
+              <MenuItem onClick={() => signOut()} color="red.500" icon={<MdLogout fontSize="26px" color="red" />}>
+                Sign out
+              </MenuItem>
+            }
+
+            {!session &&
+              <MenuItem onClick={() => signIn()} color="green.400" icon={<MdLogin fontSize="26px" color="green" />}>
+                Log In
+              </MenuItem>
+            }
+          </MenuList>
+        </Menu>
+      </HStack >
+    );
+  }
+
+  const DesktopNav = () => {
+    return (
+      <HStack alignItems="center" justify="space-between" h="100%">
+        <HStack spacing={4}>
+          <NextLink href='/' passHref>
+            <Text
+              fontSize="4xl"
+              letterSpacing={10}
+              _hover={{
+                color: 'gray.600'
+              }}
+              cursor="pointer"
+            >
+              Scalor
+            </Text>
+          </NextLink>
+          <NextLink href='/' passHref>
+            <Text
+              fontSize="4xl"
+              color='red.400'
+              fontWeight="bold"
+              cursor="pointer"
+            >
+              (BETA)
+            </Text>
+          </NextLink>
+
+          <NextLink href='/docs/upload' passHref>
+            <Button bg="none">
+              Documentation
+            </Button>
+          </NextLink>
+          <NextLink href='/demo/resize' passHref>
+            <Button bg="none">
+              Demo
+            </Button>
+          </NextLink>
+          {/* <NextLink href='/pricing' passHref>
           <Button bg="none">
             Pricing
           </Button>
         </NextLink> */}
-        <NextLink href='mailto:scalor@protonmail.com'>
-          <Button bg="none">
-            Support
-          </Button>
-        </NextLink>
-      </HStack>
+          <NextLink href='mailto:scalor@protonmail.com'>
+            <Button bg="none">
+              Support
+            </Button>
+          </NextLink>
+        </HStack>
 
-      <HStack spacing={4}>
-        <UserSession />
+        <HStack spacing={4}>
+          <DesktopUserSession />
+        </HStack>
       </HStack>
-    </HStack >
+    );
+  }
+
+  return (
+    <>
+      <Box
+        display={{
+          base: 'none',
+          lg: 'block'
+        }}
+        pt="2 "
+      >
+        <DesktopNav />
+      </Box>
+      <Box
+        display={{
+          base: 'block',
+          lg: 'none'
+        }}
+        pt="2 "
+      >
+        <MobileNav />
+      </Box>
+    </>
   );
 }
