ddf15a5f1bcc96235e1cf0ff32c9988845aa253b
trun222 <thomasunderwoodii@gmail.com>
2022-12-08 10:31:09 -0500

    Attempt to fix CORS

--- src/loaders/httpLoader.ts
+++ src/loaders/httpLoader.ts
@@ -5,6 +5,7 @@ export default async function httpLoader({ route, payload, token }: any) {
     return (await axios
       .post(`${process?.env?.NEXT_PUBLIC_API_ENDPOINT}/${route}`, payload, {
         headers: {
+          "Access-Control-Allow-Origin": "*",
           "Content-Type": "multipart/form-data",
           token,
         }
